e483e2ad6288f2035eb7b33ceab8c28744cf7ec8
Anna Zemliachenko <Anna_Zemliachenko@epam.com>
2019-04-09 23:56:06 +0300

    test radio components

--- src/components/radio/radio.test.js
+++ src/components/radio/radio.test.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import Radio from './radio.jsx';
-import { mount } from "enzyme";
+import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
 
 describe('Radio', function() {
-	it('test chooseSort', () => {
+	it('check if radio component render properly',() => {
+		const wrapper = shallow(<Radio id="sortReleaseDate" name="sort" value="releaseDate" labelText="release date" />);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('test chooseSort with releaseDate value', () => {
 		const onChange = jest.fn(),
-			props = {
-				value: "releaseDate",
-				onChange
-			},
-			component = mount(<Radio {...props} />).find("input");
-		component.simulate("change");
-		expect(onChange).toHaveBeenCalledWith("releaseDate");
+			component = shallow(<Radio onChange={onChange} />);
+		component.find("input").simulate("change","releaseDate");
+		expect(onChange).toBeCalledWith("releaseDate");
+	});
+
+	it('test chooseSort with rating value', () => {
+		const onChange = jest.fn(),
+			component = shallow(<Radio onChange={onChange} />);
+		component.find("input").simulate("change","rating");
+		expect(onChange).toBeCalledWith("rating");
+	});
+
+	it('test changeSearchFilter with genre value', () => {
+		const onChange = jest.fn(),
+			component = shallow(<Radio onChange={onChange} />);
+		component.find("input").simulate("change","genre");
+		expect(onChange).toBeCalledWith("genre");
+	});
+
+	it('test changeSearchFilter with title value', () => {
+		const onChange = jest.fn(),
+			component = shallow(<Radio onChange={onChange} />);
+		component.find("input").simulate("change","title");
+		expect(onChange).toBeCalledWith("title");
 	});
 });
