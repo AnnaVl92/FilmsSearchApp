b30b31c9129c258bb21590529a1af5a5deab32c8
Anna Zemliachenko <Anna_Zemliachenko@epam.com>
2019-04-28 21:46:24 +0300

    added beginning of offline storage (comments)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,7 +8,7 @@ class App extends React.Component {
 	render(){
 		return (
 			<ErrorBoundary>
-				<FilmPage />
+				<Search />
 			</ErrorBoundary>
 		)
 	}
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
+// import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
 import reducer from '../reducers';
 import { logger } from 'redux-logger';
 import rootSaga from '../sagas';
 
-const rootReducer = function(){};
+// const persistConfig = {
+// 	key: 'root',
+// 	storage
+// };
+
+// const persistedReducer = persistReducer(persistConfig, reducer);
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
