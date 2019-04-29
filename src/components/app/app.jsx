bfe4678087c501ae2efcd8873389da020b84f957
Anna Zemliachenko <Anna_Zemliachenko@epam.com>
2019-04-29 23:28:10 +0300

    router beginning

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,12 +3,27 @@ import css from './app.css';
 import Search from '../search/search.jsx';
 import FilmPage from '../filmPage/filmPage.jsx';
 import ErrorBoundary from '../error/errorBoundary.jsx';
+import NoFilmsFound from  '../films/noFilmsFound.jsx';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const Main = () => {
+	return (
+		<Switch>
+			<Route exact path="/" component={Search}/>
+			<Route path="/film/:id" component={FilmPage}/>
+			<Route component={NoFilmsFound}/>
+		</Switch>
+	)
+};
+
 
 class App extends React.Component {
 	render(){
 		return (
 			<ErrorBoundary>
-				<Search />
+				<Router>
+					<Route exact path="/" component={Search}/>
+				</Router>
 			</ErrorBoundary>
 		)
 	}
