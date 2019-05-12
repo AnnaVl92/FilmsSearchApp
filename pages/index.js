c8f10e2b2c0a0172c6ce47cbebb01cac5ebcee18
Anna Zemliachenko <Anna_Zemliachenko@epam.com>
2019-05-12 22:32:07 +0300

    my attempts

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import App from '../src/components/app/app.jsx';
 import { persistor, store } from '../src/redux/store/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 import ErrorBoundary from '../src/components/error/errorBoundary.jsx';
+import fetch from 'isomorphic-fetch';
 
 function Index(props) {
 	return (
@@ -15,6 +16,13 @@ function Index(props) {
 			</ErrorBoundary>
 		</Provider>
 	)
+};
+
+Index.getInitialProps = async function() {
+	const res = await fetch('https://reactjs-cdp.herokuapp.com/movies');
+	const data = await res.json();
+
+	console.log(res);
 }
 
 export default Index;
